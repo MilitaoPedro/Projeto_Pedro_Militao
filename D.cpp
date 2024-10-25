30898d875760976810e1107d269df71e378a2a1b
Pedro Militao <102882308+MilitaoPedro@users.noreply.github.com>
2024-10-25 17:47:05 -0300

    Created: Nova classe D

---
+++ D.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+class D {
+    private:
+        int D1;
+        float D2;
+    public:
+        D(int D1, float D2);
+        ~D();
+        void MD1();
+        void MD2();
+};
+
+D::D(int D1, float D2){
+    this->D1 = D1;
+    this->D2 = D2;
+}
+
+void D::MD1(){
+    std::cout << "MD1\n";
+}
+
+void D::MD2(){
+    std::cout << "MD2\n";
+}
