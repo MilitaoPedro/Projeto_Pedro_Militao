1f0b0b34b658fa24e8ab26eeaadc3a57a93def74
Pedro Militao <102882308+MilitaoPedro@users.noreply.github.com>
2024-10-25 17:16:59 -0300

    Criação da Classe C

---
+++ C.cpp
@@ -0,0 +1,26 @@
+#include <string>
+#include <iostream>
+
+class C {
+    private:
+        std::string C1;
+        int C2;
+    public:
+        C(std::string C1, int C2);
+        ~C();
+        void MC1();
+        void MC2();
+};
+
+C::C(std::string C1, int C2) {
+    this->C1 = C1;
+    this->C2 = C2;
+}
+
+void C::MC1() {
+    std::cout << "MC1\n";
+}
+
+void C::MC2() {
+    std::cout << "MC2\n";
+}
